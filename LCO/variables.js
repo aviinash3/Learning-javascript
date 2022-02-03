b412a70ada06c97cd6dc9d81882ee6afc6d2bf46
avinash <aviikr3@gmail.com>
2022-02-03 16:36:07 +0530

    interopoletion

--- LCO/variables.js
+++ LCO/variables.js
@@ -3,7 +3,7 @@ var fullName = "avinash kr"; //variabble
 var courseName = "lets lern javascript";
  var ishere = true;  //boolian variabe
  var ishere = false ; //boolian variabe
- var number = 89; //string variable
+ var number = 89; //number variable
  var payment;
  payment = "UPi"
 
@@ -15,3 +15,20 @@ console.log (ishere);
 console.log (payment);
 console.log (number);
 
+//////////////
+const uid = "apple"; // you cant change variable leter on. 
+var fullName = "avinash kumar";
+var email = "abc@gmail.com";
+var islogedin = true
+var userage = 40 
+var useremainisvalid= true
+ 
+
+///// another waiy for loging with back tick (`) // this method is called interpoletion.
+console.log(`
+with unique is : ${uid}
+user is : ${fullName}
+user age is : ${userage}
+user email is ${email}
+${useremainisvalid}
+`)
