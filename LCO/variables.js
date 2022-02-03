b6218e97aae3c718ddac94f3adbf163e8d23b4d5
avinash <aviikr3@gmail.com>
2022-02-03 16:17:56 +0530

    variables

---
+++ LCO/variables.js
@@ -0,0 +1,17 @@
+console.log ("avinash");
+var fullName = "avinash kr"; //variabble
+var courseName = "lets lern javascript";
+ var ishere = true;  //boolian variabe
+ var ishere = false ; //boolian variabe
+ var number = 89; //string variable
+ var payment;
+ payment = "UPi"
+
+
+console.log (fullName);
+
+console.log ("fullName");
+console.log (ishere);
+console.log (payment);
+console.log (number);
+
